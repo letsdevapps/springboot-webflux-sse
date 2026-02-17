e0afeefa8201da6b41bbbeea9b8d16eafbd8dc74
letsdevapps <letsdevapps@gmail.com>
2026-02-17 15:43:30 -0300

    Inclusao alternativo para sse streaming

--- src/main/java/com/pro/api/SseApi.java
+++ src/main/java/com/pro/api/SseApi.java
@@ -1,11 +1,16 @@
 package com.pro.api;
 
+import java.time.Duration;
 import java.util.concurrent.Executors;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import jakarta.servlet.http.HttpServletResponse;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/sse")
@@ -26,5 +31,19 @@ public class SseApi {
 			}
 		});
 		return emitter;
+	}
+
+	@GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamIndex() {
+        return Flux.interval(Duration.ofSeconds(1))
+                   .map(seq -> "Stream Index" + seq);
+    }
+	
+	@GetMapping(value = "/stream-2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> streamIndex2(HttpServletResponse response) {
+	    response.setHeader("Cache-Control", "no-store, must-revalidate");
+	    response.setHeader("Pragma", "no-cache");
+	    return Flux.interval(Duration.ofSeconds(1))
+	               .map(seq -> "Stream Index 2 " + seq);
 	}
 }
